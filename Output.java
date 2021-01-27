5b87526a120c4e7e786a1c1b8fe49ffd010160f0
l <l>
2021-01-27 22:41:17 +0900

    最新版

--- Output.java
+++ Output.java
@@ -3,7 +3,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 //import java.util.Random;
-
+//適当
 public class Output {
     public static void main(String[] args) {
         Currentdate();
